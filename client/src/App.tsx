394aca2abb7a1d74c021082d7bb8c5afb27132e9
Su Hlaing <suhlaing200341@gmail.com>
2023-10-15 20:26:24 +0630

    feat: start project

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-3xl  font-bold ">Hello World........</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/NavaBar.tsx
@@ -0,0 +1,3 @@
+export default function NavaBar() {
+  return <div></div>;
+}
