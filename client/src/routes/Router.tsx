c14de1e5fc165d47e224d3f8325139c08d7a5caf
Su Hlaing <suhlaing200341@gmail.com>
2023-11-05 19:07:16 +0630

    feat: add contact page

--- client/src/routes/Router.tsx
+++ client/src/routes/Router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavaBar from "../components/navaBar/NavaBar";
 import SinglePost from "../components/singlePost/SinglePost";
 import About from "../pages/about/About";
+import Contact from "../pages/contact/Contact";
 import Home from "../pages/home/Home";
 import Settings from "../pages/settings/Settings";
 import SignIn from "../pages/signIn/SignIn";
@@ -22,6 +23,7 @@ export default function Router() {
         <Route path="/posts/:postId" Component={user ? SinglePost : SignUp} />
         <Route path="/about" Component={user ? About : SignUp} />
         <Route path="/write" Component={user ? CreatePost : SignUp} />
+        <Route path="/contact" Component={user ? Contact : SignUp} />
         <Route path="/settings" Component={user ? Settings : SignUp} />
       </Routes>
     </BrowserRouter>
