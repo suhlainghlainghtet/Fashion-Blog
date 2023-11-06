9b93ad4441888081d0f7934d25f6136466aa8cf8
Su Hlaing <suhlaing200341@gmail.com>
2023-11-06 09:39:55 +0630

    chores: add active nava items and remove some

--- client/src/components/mobileNavaBar/MobileNavaBar.tsx
+++ client/src/components/mobileNavaBar/MobileNavaBar.tsx
@@ -3,12 +3,25 @@ import cardImg2 from "../../assets/cardImg2.jpg";
 interface Prop {
   open: boolean;
   setOpen: (value: boolean) => void;
+  activeNavItem: null | string;
+  setActiveNavItem: (value: string) => void;
 }
-const MobileNavaBar = ({ open, setOpen }: Prop) => {
+const MobileNavaBar = ({
+  open,
+  setOpen,
+  activeNavItem,
+  setActiveNavItem,
+}: Prop) => {
   const openNavBarFormobile = () => {
     setOpen(true);
   };
   const closeNavBarFormobile = () => {
+    setOpen(false);
+  };
+
+  // ===============Active Nava Item============
+  const handleActiveNavItemClick = (item: string) => {
+    setActiveNavItem(item);
     setOpen(false);
   };
   return (
@@ -27,7 +40,6 @@ const MobileNavaBar = ({ open, setOpen }: Prop) => {
           <i className="ri-search-line text-[1.8rem] cursor-pointer  text-dark-blue"></i>
         </div>
       </div>
-
       <div
         className={
           open
@@ -59,36 +71,50 @@ const MobileNavaBar = ({ open, setOpen }: Prop) => {
           onClick={closeNavBarFormobile}
           className="ri-close-fill  text-[2.3rem] top-3 text-dark-blue cursor-pointer absolute right-[5%]"
         ></i>
-
         <ul
           className=" flex flex-col 360:gap-y-[1.6rem] gap-y-[1rem] px-5 text-dark-blue
          font-navItemsForfont uppercase text-lg cursor-pointer pt-[9rem] 360:pt-[10rem]"
         >
           <span className="flex gap-2 border-b-2 pb-3 360:pb-4 w-full">
-            <i className="ri-home-fill text-xl"></i>
-            <li>
-              <Link to="/">home</Link>
-            </li>
+            <Link to="/">
+              <li
+                className={activeNavItem === "home" ? "active " : ""}
+                onClick={() => handleActiveNavItemClick("home")}
+              >
+                <i className="ri-home-fill text-xl"></i> home
+              </li>
+            </Link>
+          </span>
+          <span className="flex gap-2 border-b-2 pb-3 360:pb-4 w-full">
+            <Link to="/about">
+              <li
+                className={activeNavItem === "about" ? "active" : ""}
+                onClick={() => handleActiveNavItemClick("about")}
+              >
+                <i className="ri-group-fill text-xl"></i> about
+              </li>
+            </Link>
+          </span>
+          <span className="flex gap-2 border-b-2 pb-3 360:pb-4 w-full">
+            <Link to="/write">
+              <li
+                className={activeNavItem === "write" ? "active" : ""}
+                onClick={() => handleActiveNavItemClick("write")}
+              >
+                <i className="ri-contacts-book-2-fill text-xl"></i> write
+              </li>
+            </Link>
           </span>
 
           <span className="flex gap-2 border-b-2 pb-3 360:pb-4 w-full">
-            <i className="ri-group-fill text-xl"></i>
-            <li>
-              <Link to="/about">about</Link>
-            </li>
-          </span>
-
-          <span className="flex gap-2 border-b-2 pb-3 360:pb-4 w-full">
-            <i className="ri-contacts-book-2-fill text-xl"></i>
-            <li>
-              <Link to="/write">write</Link>
-            </li>
-          </span>
-          <span className="flex gap-2 border-b-2 pb-3 360:pb-4 w-full">
-            <i className="ri-pencil-fill text-xl"></i>
-            <li>
-              <Link to="/contact">contact</Link>
-            </li>
+            <Link to="/contact">
+              <li
+                className={activeNavItem === "contact" ? "active" : ""}
+                onClick={() => handleActiveNavItemClick("contact")}
+              >
+                <i className="ri-pencil-fill text-xl"></i> contact
+              </li>
+            </Link>
           </span>
           <span className="flex gap-2 border-b-2 pb-3 360:pb-4 w-full">
             <i className="ri-logout-box-r-fill text-xl"></i> <li>logout</li>
